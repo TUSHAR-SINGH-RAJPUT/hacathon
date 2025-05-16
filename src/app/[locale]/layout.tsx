b308ea544a99a0eef9ad15aaf054b61fd048184b
TUSHAR SINGH <tusharsinghh8@gmail.com>
2025-05-16 14:47:57 +0000

    fix error

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,3 +2,13 @@
 // As i18n is being reverted, this file is no longer needed.
 // Its logic has been merged back into /src/app/layout.tsx.
 // Please delete the /src/app/[locale] directory.
+
+import React from 'react';
+
+export default function ObsoleteRootLayoutForLocale({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
